b92542c7c99585bcb51feef6f30baef916799d65
ccahui <kris_ti4n@hotmail.com>
2019-02-10 21:55:06 -0500

    Trabajando la DB en cloud Mongo db y HEROKU

--- server/config/config.js
+++ server/config/config.js
@@ -14,11 +14,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 // =======================================
 
 let URI;
-//if (process.env.NODE_ENV === 'local') {
- //   URI = 'mongodb://localhost:27017/cafe';
-//} else {
+if (process.env.NODE_ENV === 'local') {
+    URI = 'mongodb://localhost:27017/cafe';
+} else {
     //Especificamos /cafe ---> Nombre de la base de datos en la nube
     URI = 'mongodb+srv://cafe-user:mongodb123456@cluster0-ox43o.mongodb.net/cafe?retryWrites=true';
-//}
+}
 
 process.env.URI_DB = URI;
