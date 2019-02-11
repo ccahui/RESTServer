5ee3fb0adedfb34f4f2ad29bc5b2e9df469d34c7
ccahui <kris_ti4n@hotmail.com>
2019-02-10 22:21:28 -0500

    Colocando un process.env.NODE_ENV para trabajar en local y nube y saber a cual BD conectarme

--- server/config/config.js
+++ server/config/config.js
@@ -14,15 +14,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 // =======================================
 
 let URI;
-//if (process.env.NODE_ENV === 'local') {
-//    URI = 'mongodb://localhost:27017/cafe';
-//} else {
+if (process.env.NODE_ENV === 'local') {
+   URI = 'mongodb://localhost:27017/cafe';
+} else {
     //Especificamos /cafe ---> Nombre de la base de datos en la nube
     // =======================================
     //  EN MONGO BD ATLAS LISTA DE DIRECCION IP DE CONEXION PERMITIDAS 0.0.0.0/0
     // =======================================
     URI = 'mongodb+srv://cafe-user:mongodb123456@cluster0-ox43o.mongodb.net/cafe?retryWrites=true';
-//}
+}
 
 
 
